e6be67af3318af355649007aa381beb7a3367c54
hafei <zhangmengcoding@gmail.com>
2019-04-07 16:38:36 +0800

    添加项目文件。

---
+++ Ebooks.Allitebooks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ebooks.Allitebooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
